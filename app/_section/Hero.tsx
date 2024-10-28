1bcb31e33c8012a0284224981049f5ae02695fed
Shirin <shiringohariii@gmail.com>
2024-10-28 23:44:30 +0330

    feat: add stickey bottom navbar

---
+++ app/_section/Hero.tsx
@@ -0,0 +1,19 @@
+function Hero() {
+  return (
+    <div
+      className="border border-black h-[70vh]
+ flex flex-col  justify-center  items-center content-center gap-10 w-100  "
+    >
+      <p className="text-black font-bold flex flex-col justify-center items-center text-5xl  md:text-7xl">
+        <span className="text-center">Unlock Opportunities,</span>
+        <span className="text-center">Pursue Your Passion</span>
+      </p>
+      <h1 className="text-black text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+        magni.
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ app/components/navigation/TopNavigationDesktop.tsx
@@ -0,0 +1,45 @@
+function TopNavigationDesktop() {
+  return (
+    <div>
+        <nav className="text-lg">
+            <div className="flex flex-row justify-between px-4 py-5 mx-auto">
+                <div className="flex items-center">
+                    <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse ">
+                    <li>
+                            <a href="#" className="text-black  hover:underline" aria-current="page">Logo😊</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black  hover:underline" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black  hover:underline">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black  hover:underline">Team</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black  hover:underline">Support</a>
+                        </li>
+                    </ul>
+                </div>
+                {/* <div className="flex items-center">
+                    <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse ">
+                        <li>
+                            <a href="#" className="text-black" aria-current="page">Sign in</a>
+                        </li>
+                        <li>
+                            <button >
+                            <a href="#" className="text-black  ">Sign up</a>
+                            </button>
+                        </li>
+                    
+                    </ul>
+                </div> */}
+            </div>
+           
+        </nav>
+    </div>
+  )
+}
+
+export default TopNavigationDesktop
